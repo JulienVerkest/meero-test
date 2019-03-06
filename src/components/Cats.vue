a6c4991f3a5c132ab9ecfe6bd04a1ba3161ca579
Julien Verkest <julienverkest@gmail.com>
2019-03-06 14:34:59 +0100

    first commit test technique list of cats

---
+++ src/components/Cats.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <b-container fluid>
+      <b-navbar toggleable="md" type="dark" variant="primary">
+        <b-navbar-brand href="#">THE CAT WEBSITE</b-navbar-brand>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown
+              id="nav7_ddown"
+              text="Filtre par origine"
+              extra-toggle-classes="nav-link-custom"
+              right
+            >
+              <b-dropdown-item v-for="origin in filters">{{origin}}</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+      </b-navbar>
+    </b-container fluid>
+    <b-container class="mt-4">
+      <b-card-group v-for="cats in groupCats" deck>
+        <b-card v-for="(cat, i) in cats" :title="cat.name" :img-src="cat.thumbnail" img-alt="" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+          <b-card-text>
+            {{cat.origin}}
+          </b-card-text>
+          <b-button href="#" variant="primary" class="float-right">Détails</b-button>
+        </b-card>
+      </b-card-group>
+    </b-container>
+  </div>
+</template>
+<script>
+  import _ from 'lodash'
+  import axios from 'axios';
+  export default {
+    name: 'Cats',
+    data () {
+      return {
+        groupCats: [],
+        filters:[] 
+      }
+    },
+    mounted() {
+      this.listCats();
+    },
+    methods: {
+      async listCats () {
+        try {
+          const data = await axios.get('https://api.thecatapi.com/v1/breeds', {'x-api-key': '065ec24f-44b5-4ddb-8090-bb36eb150183'})
+          // Thumbnail, Description
+          let that = this
+          _.each(data.data,function(cat){
+            that.filters.push(cat.origin)
+          })
+
+          Promise.all(data.data.map(cat => {
+            return this.details(cat)
+          })).then(details => {
+              details.forEach((cat, i) => {
+                if(cat) {
+                  data.data[i].thumbnail = cat.thumbnail
+                  data.data[i].description = cat.description
+                }
+              });
+            this.groupCats = _.chunk(data.data, 3)
+          })
+        } catch (err) {
+          console.error(err);
+        }
+      },
+      async details(cat) {
+        try {
+          let url_wiki
+          let details = {
+            thumbnail: '',
+            description: ''
+          }
+          if(cat.wikipedia_url) {
+            url_wiki = cat.wikipedia_url.split('/')
+            const responseMedia = await axios.get(`https://en.wikipedia.org/api/rest_v1/page/media/${url_wiki[url_wiki.length - 1]}`)
+            if (responseMedia.data.items[0] && responseMedia.data.items[0].thumbnail) {
+              details.thumbnail = responseMedia.data.items[0].thumbnail.source
+            }
+            if (responseMedia.data.items[0] && responseMedia.data.items[0].description) {
+              details.description = responseMedia.data.items[0].description.text
+            }
+          }
+          return details;
+        } catch (err) {
+          console.error("Error wikipedia", err);
+        }
+      }
+    }
+  }
+</script>
+<style>
+.card-img-top {
+  overflow:hidden;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Cats from './components/Cats.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(Router)
+Vue.use(BootstrapVue)
+// Vue.use(Vuex)
 
 export default new Router({
   mode: 'history',
@@ -10,16 +16,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      name: 'cats',
+      component: Cats
     }
   ]
 })
